e13a804cfc10c29ad7a80c37419c7d33c3f0ec27
jxd <1053376250@qq.com>
2019-02-26 23:19:54 +0800

    init

--- src/vNode/patch/index.ts
+++ src/vNode/patch/index.ts
@@ -1,4 +1,6 @@
 
-function patch(vNode: VNode, isRoot: Boolean): DocumentFragment {
+//传入新老node
+function patch(vNode: VNode,oldNode:VNode, isRoot: Boolean): void {
+	//当vnode渲染时候从data中获取数据的时候进行添加vnode更改的依赖，当数据改变的时候将数据改变压入到队列中去，等待一个事件循环周期调用执行循环玩了再patch
 	
 }
